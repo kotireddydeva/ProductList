3c136e620907d183b39023a30c5c635252bfee01
koti <devakoti.fullstackdeveloper@gmail.com>
2025-09-03 17:41:49 +0530

    package added

--- src/DynamicForm.tsx
+++ src/DynamicForm.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
 import type { FC } from "react";
 
 
